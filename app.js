155621b2db0f07b81abc0f305e61cf5211ebe396
Edita Ignot <edignot@Editas-MacBook-Pro.local>
2022-05-11 17:04:10 -0600

    Initial commit

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 require('dotenv').config()
 
+// 1. Install and import WorkOS SDK
 const WorkOS = require('@workos-inc/node').default
+// 2. Initialize WorkOS client with WorkOS API Key
 const workos = new WorkOS(process.env.WORKOS_API_KEY)
+// 3. Add Client ID that is specific to environment in WorkOS dashboard
 const clientID = process.env.WORKOS_CLIENT_ID
 
 const app = express()
@@ -24,24 +27,32 @@ app.get('/', (req, res) => {
 app.get('/login', (_req, res) => {
   try {
     const authorizationURL = workos.sso.getAuthorizationURL({
-      connection: 'conn_01G287ZATJG4AX65S19FKFWQ90',
+      // 4. Connection ID associated with SSO Connection in WorkOS dashboard
+      connection: 'conn_01G2TM1BYXCPFB5Y12WN7FK2DY',
       clientID: clientID,
-      redirectURI: 'https://node-ejs-okta-sso-workos.herokuapp.com/callback',
+      // 5. Endpoint that handles callback from WorkOS when user authenticates, optional, if not provided it will use default from WorkOS dashboard
+      redirectURI: 'http://localhost:3000/callback',
     })
+    // 6. Redirect user to uthorization URL
     res.redirect(authorizationURL)
   } catch (error) {
-    res.render('index', { text: error })
+    console.log(error)
+    res.render('index')
   }
 })
 
 app.get('/callback', async (req, res) => {
+  // 7. Exchange authorization code for the user profile
   const { code } = req.query
+  console.log('code', req.query)
+  // 8. Make a call to WorkOS API to get user profile
   const { profile } = await workos.sso.getProfileAndToken({
     code,
     clientID: clientID,
   })
 
   user = profile
+  // 9. Other logic after user successfully authenticates
   res.redirect('/account')
 })
 
